85ef012d8d0debe2c41521d5aa885f038c06f432
tsgreenberg1217 <tsgreenberg1217@gmail.com>
2018-03-11 21:55:41 -0400

    added client

--- playground/dbTesting.js
+++ playground/dbTesting.js
@@ -3,11 +3,15 @@ const {Question} = require('../models/question')
 const {User} = require('../models/user')
 const {Session} = require('../models/session')
 
+
+// function for inserting into subarray
 const test = async function(){
   let qs = await Question.find()
   let q1 = qs[0]
   let newSession = new Session()
+  // start new game
   newSession.results.push({question: q1})
+  // when they answer
   newSession.results[0].result = true
   console.log(newSession.results[0])
   let s = await newSession.save()
